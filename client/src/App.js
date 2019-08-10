35d08ecd9d82f728ace2b17373bb82cb949aace7
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-10 15:36:40 -0500

    installed react-router-dom module

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 class App extends Component {
   state = {
-    courses : [],
-    isLoading : true,
+    baseURL: "http://localhost:5000/api",
+    courses: [],
+    isLoading: true,
   }
+
   componentDidMount() {
-    fetch(`http://localhost:5000/api/courses`)
+    fetch(`${this.state.baseURL}/courses`)
       .then(response => response.json())
       .then(courses => this.setState({
         courses : courses,
@@ -16,23 +18,28 @@ class App extends Component {
       .then(() => console.log(this.state.courses))
       .catch(err => console.log("There was an error loading the courses: " + err))
   }
+
   render() {
     return this.state.isLoading ? (<h2>Loading...</h2>) : (
-      <div className="container">
-        {
-          this.state.courses.map(course => {
-            return (
-              <ul key={course.id}>
-                <li>{course.id}</li>
-                <li>{course.title}</li>
-                <li>{course.description}</li>
-                <li>{course.estimatedTime}</li>
-                <li>{course.materialsNeeded}</li>
-              </ul>
-            );
-          })
-        }
+      <BrowserRouter>
+      <div id="root">
+        <div>
+          {
+            this.state.courses.map(course => {
+              return (
+                <ul key={course.id}>
+                  <li>{course.id}</li>
+                  <li>{course.title}</li>
+                  <li>{course.description}</li>
+                  <li>{course.estimatedTime}</li>
+                  <li>{course.materialsNeeded}</li>
+                </ul>
+              );
+            })
+          }
+        </div>
       </div>
+      </BrowserRouter>
     );
   }
 }
