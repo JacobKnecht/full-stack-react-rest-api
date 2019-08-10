481be6bc8108a602df5caea5dd48578b69e370fd
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-10 13:29:30 -0500

    implemented placeholder course render on home page load

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    courses : [],
+    isLoading : true,
+  }
+  componentDidMount() {
+    fetch(`http://localhost:5000/api/courses`)
+      .then(response => response.json())
+      .then(courses => this.setState({
+        courses : courses,
+        isLoading : false
+      }))
+      .then(() => console.log(this.state.courses))
+      .catch(err => console.log("There was an error loading the courses: " + err))
+  }
+  render() {
+    return this.state.isLoading ? (<h2>Loading...</h2>) : (
+      <div className="container">
+        {
+          this.state.courses.map(course => {
+            return (
+              <ul key={course.id}>
+                <li>{course.id}</li>
+                <li>{course.title}</li>
+                <li>{course.description}</li>
+                <li>{course.estimatedTime}</li>
+                <li>{course.materialsNeeded}</li>
+              </ul>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
