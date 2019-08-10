674aab51cb5478d13980cc04f0fda8d9b86baacc
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-10 17:54:31 -0500

    set homepage route to render the <Courses> component/courses list

--- client/src/App.js
+++ client/src/App.js
@@ -2,34 +2,32 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch, Link } from 'react-router-dom';
 
 import Header from './components/Header';
+import Courses from './components/Courses';
 import NotFound from './components/NotFound';
 
 class App extends Component {
   state = {
     baseURL: "http://localhost:5000/api",
-    courses: [],
-    isLoading: true,
-  }
-
-  componentDidMount() {
-    fetch(`${this.state.baseURL}/courses`)
-      .then(response => response.json())
-      .then(courses => this.setState({
-        courses : courses,
-        isLoading : false
-      }))
-      .then(() => console.log(this.state.courses))
-      .catch(err => console.log("There was an error loading the courses: " + err))
   }
 
   render() {
-    return this.state.isLoading ? (<h2>Loading...</h2>) : (
+    return this.state.isLoading ? (<h2>Loading Courses...</h2>) : (
       <BrowserRouter>
       <div id="root">
         <div>
           <Header />
           <hr />
           <Switch>
+            {/*Homepage Route - Renders <Courses> Component/Courses List*/}
+            <Route
+              exact path="/"
+              render={ () =>
+                <Courses
+                  baseURL={this.state.baseURL}
+                  toggleLoad={this.toggleLoad}
+                />
+              }
+            />
             {/*404/Not Found Route*/}
             <Route component={NotFound} />
           </Switch>
