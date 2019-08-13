548fa3f8a9ead11428e2b66caec85ecb14d9f181
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-13 15:18:13 -0500

    created <UserSignIn> component

---
+++ client/src/components/UserSignIn.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class UserSignIn extends Component {
+  state = {
+    userEmailAddress: "",
+    userPassword: "",
+  };
+
+  returnToList(e) {
+    e.preventDefault();
+    window.location.assign("/");
+  }
+
+  render() {
+    return (
+      <div className="bounds">
+        <div className="grid-33 centered signin">
+          <h1>Sign In</h1>
+          <div>
+            <form>
+              <div>
+                <input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" />
+              </div>
+              <div>
+                <input id="password" name="password" type="password" className="" placeholder="Password" value="" />
+              </div>
+              <div class="grid-100 pad-bottom">
+                <button className="button" type="submit">Sign In</button>
+                <button className="button button-secondary" onClick={this.returnToList}>Cancel</button>
+              </div>
+            </form>
+          </div>
+          <p>&nbsp;</p>
+          <p>Don't have a user account? <Link to="sign-up.html">Click here</Link> to sign up!</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default UserSignIn;
