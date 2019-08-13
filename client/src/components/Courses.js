27dc94d4f2f036abb748c24177224245dd919231
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-13 17:00:28 -0500

    added path to create course button link

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -30,7 +30,7 @@ class Courses extends Component {
           })
         }
         <div className="grid-33">
-          <Link to="#" className="course--module course--add--module">
+          <Link to="/courses/create" className="course--module course--add--module">
             <h3 className="course--add--title">
               <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px"
                 y="0px" viewBox="0 0 13 13" className="add">
