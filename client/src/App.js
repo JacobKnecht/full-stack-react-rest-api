782b40ea582e5dd5ffdc7f627aae2c1edd3d2e6e
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-15 00:11:45 -0500

    created routes for unhandled error and forbidden/unauthorized access pages

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,10 @@ import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
 import UserSignOut from './components/UserSignOut';
 import NotFound from './components/NotFound';
+import UnhandledError from './components/UnhandledError';
+import Forbidden from './components/Forbidden';
 
+import { Provider } from './components/Context'
 import withContext from './components/Context';
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignUpWithContext = withContext(UserSignUp);
@@ -23,54 +26,60 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-      <div id="root">
-        <div>
-          <Header />
-          <hr />
-          <Switch>
-            {/*Homepage/Course List Route*/}
-            <Route
-              exact path="/"
-              render={ () => <Courses baseURL={this.state.baseURL} /> }
-            />
-            {/*Create Course Route*/}
-            <Route
-              path="/courses/create"
-              render={ () => <CreateCourse baseURL={this.state.baseURL} /> }
-            />
-            {/*Course Update Route*/}
-            <Route
-              path="/courses/:id/update"
-              render={ props =>
-                <UpdateCourse
-                  {...props}
-                  baseURL={this.state.baseURL}
-                />
-              }
-            />
-            {/*Course Details Route*/}
-            <Route
-              path="/courses/:id"
-              render={ props =>
-                <CourseDetail
-                  {...props}
-                  baseURL={this.state.baseURL}
-                />
-              }
-            />
-            {/*User Sign-In Route*/}
-            <Route path="/signin" component={UserSignInWithContext} />
-            {/*User Sign-Up Route*/}
-            <Route path="/signup" component={UserSignUpWithContext} />
-            {/*User Sign-Out Route*/}
-            <Route path="/signout" component={UserSignOut} />
-            {/*404/Not Found Route*/}
-            <Route component={NotFound} />
-          </Switch>
+      <Provider>
+        <BrowserRouter>
+        <div id="root">
+          <div>
+            <Header />
+            <hr />
+            <Switch>
+              {/*Homepage/Course List Route*/}
+              <Route
+                exact path="/"
+                render={ () => <Courses baseURL={this.state.baseURL} /> }
+              />
+              {/*Create Course Route*/}
+              <Route
+                path="/courses/create"
+                render={ () => <CreateCourse baseURL={this.state.baseURL} /> }
+              />
+              {/*Course Update Route*/}
+              <Route
+                path="/courses/:id/update"
+                render={ props =>
+                  <UpdateCourse
+                    {...props}
+                    baseURL={this.state.baseURL}
+                  />
+                }
+              />
+              {/*Course Details Route*/}
+              <Route
+                path="/courses/:id"
+                render={ props =>
+                  <CourseDetail
+                    {...props}
+                    baseURL={this.state.baseURL}
+                  />
+                }
+              />
+              {/*User Sign-In Route*/}
+              <Route path="/signin" component={UserSignInWithContext} />
+              {/*User Sign-Up Route*/}
+              <Route path="/signup" component={UserSignUpWithContext} />
+              {/*User Sign-Out Route*/}
+              <Route path="/signout" component={UserSignOut} />
+              {/*Unhandled Error Route*/}
+              <Route path="/error" component={UnhandledError} />
+              {/*Forbidden/Unauthorized Route*/}
+              <Route path="/forbidden" component={Forbidden} />
+              {/*404/Not Found Route*/}
+              <Route component={NotFound} />
+            </Switch>
+          </div>
         </div>
-      </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
