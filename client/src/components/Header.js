9c067fcbcc9fc80ed910f8eabe3ec3702c251b98
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-15 16:31:30 -0500

    added code conditionally render a custom welcome message

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Header = () => {
+const Header = (props) => {
+  const { context } = props;
+  const authUser = context.authenticatedUser;
   return (
     <div className="header">
       <div className="bounds">
         <h1 className="header--logo">Courses</h1>
         <nav>
-          <span>Don't Forget The Welcome Message!</span>
-          <NavLink to="/signup" className="signup">Sign Up</NavLink>
-          <NavLink to="/signin" className="signin">Sign In</NavLink>
+          {
+            authUser ?
+            <React.Fragment>
+              <span>Welcome, {authUser.firstName}!</span>
+              <NavLink to="/signout">Sign Out</NavLink>
+            </React.Fragment>
+            :
+            <React.Fragment>
+              <NavLink to="/signup" className="signup">Sign Up</NavLink>
+              <NavLink to="/signin" className="signin">Sign In</NavLink>
+            </React.Fragment>
+          }
         </nav>
       </div>
     </div>
