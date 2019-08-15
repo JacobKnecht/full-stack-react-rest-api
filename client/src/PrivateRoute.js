9ddb0a81582af4d494e515bab5d076472faed340
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-15 18:37:43 -0500

    created <PrivateRoute> higher-order component

---
+++ client/src/PrivateRoute.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { Consumer } from './components/Context';
+
+const PrivateRoute = (props) => {
+  const {
+    component: Component,
+    ...rest
+  } = props;
+  return (
+    <Consumer>
+      {
+        context => (
+          <Route
+            {...rest}
+            render={props => context.authenticatedUser ?
+              (<Component {...props} />)
+              :
+              <Redirect
+                to={{
+                  pathname: 'signin',
+                  state: { from: props.location }
+                }}
+              />
+            )}
+          />
+      )}
+    </Consumer>
+  );
+}
+
+export default PrivateRoute;
