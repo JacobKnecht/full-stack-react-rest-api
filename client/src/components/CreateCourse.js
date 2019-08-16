e6f18832f5f945c045af3bab4a572a6bbbefe1d0
Jacob Knecht <jacobknecht3@gmail.com>
2019-08-15 22:39:12 -0500

    created form submit handler method

--- client/src/components/CreateCourse.js
+++ client/src/components/CreateCourse.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import config from '../config';
 
 class CreateCourse extends Component {
   state = {
-    courseTitle: "",
-    courseDescription: "",
-    courseEstimatedTime: "",
-    courseMaterialsNeeded: "",
+    title: "",
+    description: "",
+    estimatedTime: "",
+    materialsNeeded: "",
   };
 
   returnToList = (e) => {
@@ -14,11 +15,53 @@ class CreateCourse extends Component {
   }
 
   updateCourseTitle = (e) => {
-    this.setState({ courseTitle: e.target.value });
+    this.setState({ title: e.target.value });
+  }
+
+  updateCourseDescription = (e) => {
+    this.setState({ description: e.target.value });
   }
 
   updateCourseEstimatedTime = (e) => {
-    this.setState({ courseEstimatedTime: e.target.value });
+    this.setState({ estimatedTime: e.target.value });
+  }
+
+  updateCourseMaterialsNeeded = (e) => {
+    this.setState({ materialsNeeded: e.target.value });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { context } = this.props;
+    const authUser = context.authenticatedUser;
+    const {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded
+    } = this.state;
+    const credentials = btoa(`${authUser.emailAddress}:${authUser.password}`);
+    const response = await fetch(`${config.baseURL}/courses`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': `Basic ${credentials}`,
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        estimatedTime,
+        materialsNeeded
+      }),
+    });
+    if(response.status === 201) {
+      this.props.history.push("/");
+      console.log("Course Created");
+    } else {
+      console.log(response);
+      console.log(response.status);
+      this.props.history.push("/error");
+    }
   }
 
   render() {
@@ -36,14 +79,14 @@ class CreateCourse extends Component {
               </ul>
             </div>
           </div>
-          <form action={`${this.props.baseURL}/courses`} method="post">
+          <form onSubmit={this.handleSubmit}>
             <div className="grid-66">
               <div className="course--header">
                 <h4 className="course--label">Course</h4>
                 <div>
                   <input id="title" name="title" type="text"
                     className="input-title course--title--input"
-                    placeholder="Course title..." value={this.state.courseTitle}
+                    placeholder="Course title..." value={this.state.title}
                     onChange={this.updateCourseTitle}/>
                 </div>
                 {/*Need to put authenticated user's name in the following <p> tag*/}
@@ -52,7 +95,8 @@ class CreateCourse extends Component {
               <div className="course--description">
                 <div>
                   <textarea id="description" name="description" className=""
-                    placeholder="Course description..."></textarea>
+                    placeholder="Course description..." value={this.state.description}
+                    onChange={this.updateCourseDescription}></textarea>
                 </div>
               </div>
             </div>
@@ -64,14 +108,16 @@ class CreateCourse extends Component {
                     <div>
                       <input id="estimatedTime" name="estimatedTime" type="text"
                         className="course--time--input" placeholder="Hours"
-                        value={this.state.courseEstimatedTime} onChange={this.updateCourseEstimatedTime} />
+                        value={this.state.estimatedTime} onChange={this.updateCourseEstimatedTime} />
                     </div>
                   </li>
                   <li className="course--stats--list--item">
                     <h4>Materials Needed</h4>
                     <div>
                       <textarea id="materialsNeeded" name="materialsNeeded"
-                        className="" placeholder="List materials..."></textarea>
+                        className="" placeholder="List materials..."
+                        value={this.state.materialsNeeded}
+                        onChange={this.updateCourseMaterialsNeeded}></textarea>
                     </div>
                   </li>
                 </ul>
--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import config from '../config';
 
 class UpdateCourse extends Component {
   state = {
-    course: "",
-    courseTitle: "",
-    courseEstimatedTime: "",
-    courseDescription: "",
+    id: "",
+    title: "",
+    description: "",
+    estimatedTime: "",
+    materialsNeeded: "",
     isLoading: true,
   };
 
   componentDidMount() {
-    fetch(`${this.props.baseURL}/courses/${this.props.match.params.id}`)
+    fetch(`${config.baseURL}/courses/${this.props.match.params.id}`)
       .then(response => response.json())
       .then(course => this.setState({
-        course: course[0],
+        id: course[0].id,
+        title: course[0].title,
+        description: course[0].description,
+        estimatedTime: course[0].estimatedTime,
+        materialsNeeded: course[0].materialsNeeded,
         isLoading: false
       }))
       .catch(err => console.log("There was an error loading the course: " + err))
@@ -21,48 +28,83 @@ class UpdateCourse extends Component {
 
   returnToList = (e) => {
     e.preventDefault();
-    window.location.assign("/");
+    this.props.history.push("/");
   }
 
   updateCourseTitle = (e) => {
-    this.setState({ courseTitle: e.target.value });
+    this.setState({ title: e.target.value });
   }
 
   updateCourseDescription = (e) => {
-    this.setState({ courseTitle: e.target.value });
+    this.setState({ description: e.target.value });
   }
 
   updateCourseEstimatedTime = (e) => {
-    this.setState({ courseEstimatedTime: e.target.value });
+    this.setState({ estimatedTime: e.target.value });
+  }
+
+  updateCourseMaterialsNeeded = (e) => {
+    this.setState({ materialsNeeded: e.target.value });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    const { context } = this.props;
+    const authUser = context.authenticatedUser;
+    const {
+      title,
+      description,
+      estimatedTime,
+      materialsNeeded
+    } = this.state;
+    const credentials = btoa(`${authUser.emailAddress}:${authUser.password}`);
+    const response = await fetch(`${config.baseURL}/courses/${this.state.id}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'Authorization': `Basic ${credentials}`,
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        estimatedTime,
+        materialsNeeded
+      }),
+    });
+    if(response.status === 204) {
+      this.props.history.push("/");
+      console.log("Course Updated");
+    } else {
+      console.log(response);
+      console.log(response.status);
+      this.props.history.push("/error");
+    }
   }
 
   render() {
+    const { firstName, lastName} = this.props.context.authenticatedUser;
     return (
       this.state.isLoading ? (<h2>Loading Course Information...</h2>) :
       <div className="bounds course--detail">
         <h1>Update Course</h1>
         <div>
-          <form action={`${this.props.baseURL}/courses/${this.state.course.id}`} method="put">
+          <form onSubmit={this.handleSubmit}>
             <div className="grid-66">
               <div className="course--header">
                 <h4 className="course--label">Course</h4>
                 <div>
                   <input id="title" name="title" type="text"
                     className="input-title course--title--input"
-                    placeholder="Course title..." value={this.state.courseTitle}
+                    placeholder="Course title..." value={this.state.title}
                     onChange={this.updateCourseTitle} />
                 </div>
-                <p>By {this.state.course.user.firstName} {this.state.course.user.lastName}</p>
+                <p>By {firstName} {lastName}</p>
               </div>
               <div className="course--description">
                 <div>
                   <textarea id="description" name="description" className=""
                     placeholder="Course description..."
-                    value={
-                      (this.state.course.description) ?
-                      this.state.course.description :
-                      this.state.courseDescription
-                    }
+                    value={this.state.description}
                     onChange={this.updateCourseDescription}>
                   </textarea>
                 </div>
@@ -76,11 +118,7 @@ class UpdateCourse extends Component {
                     <div>
                       <input id="estimatedTime" name="estimatedTime" type="text"
                         className="course--time--input" placeholder="Hours"
-                        value={
-                          (this.state.course.estimatedTime) ?
-                          this.state.course.estimatedTime :
-                          this.state.courseEstimatedTime
-                        }
+                        value={this.state.estimatedTime}
                         onChange={this.updateCourseEstimatedTime} />
                     </div>
                   </li>
@@ -88,9 +126,9 @@ class UpdateCourse extends Component {
                     <h4>Materials Needed</h4>
                     <div>
                       <textarea id="materialsNeeded" name="materialsNeeded"
-                        className="" placeholder="List materials...">
-                          {/*Need to find a way to break down materials needed lists*/}
-                          {this.state.course.materialsNeeded}
+                        className="" placeholder="List materials..."
+                        value={this.state.materialsNeeded}
+                        onChange={this.updateCourseMaterialsNeeded}>
                       </textarea>
                     </div>
                   </li>
@@ -108,4 +146,4 @@ class UpdateCourse extends Component {
   }
 }
 
-export default UpdateCourse;
+export default withRouter(UpdateCourse);
